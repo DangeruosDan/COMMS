c0556518e9dfba49c3be17f89d42bb1ef8e936df
DangeruosDan <danielb916e@gmail.com>
2017-08-12 03:48:16 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import socket, sys
+
+def listener(port=4444,numOfCons = 1):
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serversocket.bind((socket.gethostname(), port))
+    serversocket.listen(numOfCons)
+    return serversocket
+
+def sender(ip = "192.168.1.1", port = 4444):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip,port))
+    return s
+
+if __name__ == "__main__":
+    serversocket = listener()
+    s = sender()
+    while True:
+        (clientsocket, address) = serversocket.accept()
+        print(clientsocket, address)
