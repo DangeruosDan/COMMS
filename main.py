7fc89c4c7a005cb9908e4deb6dd47fc09459be10
DangeruosDan <danielb916e@gmail.com>
2017-08-21 16:52:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 import socket, sys
 
-def listener(port=4444,numOfCons = 3):
+def listener(port=4444,numOfCons = 5):
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.bind((socket.gethostname(), port))
     serversocket.listen(numOfCons)
     return serversocket
 
-def sender(ip = "192.168.1.1", port = 4444):
+def sender(ip, port = 4444):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((ip,port))
     return s
 
 if __name__ == "__main__":
-    serversocket = listener()
-    s = sender()
     while True:
+        print("Host connection or join connection (h/j)\n")
+        choice = input()
+        if choice[0].lower() == 'h':
+            port = input("Port: ")
+            serversocket = listener(port=int(port))
+            break
+        elif choice[0].lower() == 'j':
+            ip = input("ip: ")
+            port = input("Port: ")
+            s = sender(ip,port)
+            break
+        else:
+            print("invalid selection type 'h' or 'j'")
+    if choice[0].lower() == 'h':
         (clientsocket, address) = serversocket.accept()
         print(clientsocket, address)
+        while True:
+            clientsocket.send("Connection received".encode('ascii'))
+    else:
+        msg = s.recv(1024)
+        print(msg.decode('ascii'))
