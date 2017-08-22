a6ea7e1bdcd24193949e82fbf4b028f84b6990c1
DangeruosDan <danielb916e@gmail.com>
2017-08-21 17:34:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import socket, sys
+from threading import Thread
 
 def listener(port=4444,numOfCons = 5):
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,6 +12,14 @@ def sender(ip, port = 4444):
     s.connect((ip,port))
     return s
 
+def listen(socket,size):
+    msg = socket.recv(size)
+    print(msg.decode('ascii'))
+
+def send(socket):
+    msg = input()
+    socket.send(msg.encode('ascii'))
+
 if __name__ == "__main__":
     while True:
         print("Host connection or join connection (h/j)\n")
@@ -22,15 +31,18 @@ if __name__ == "__main__":
         elif choice[0].lower() == 'j':
             ip = input("ip: ")
             port = input("Port: ")
-            s = sender(ip,port)
+            s = sender(ip,int(port))
             break
         else:
             print("invalid selection type 'h' or 'j'")
+
     if choice[0].lower() == 'h':
         (clientsocket, address) = serversocket.accept()
         print(clientsocket, address)
-        while True:
-            clientsocket.send("Connection received".encode('ascii'))
-    else:
-        msg = s.recv(1024)
-        print(msg.decode('ascii'))
+        clientsocket.send("Connection received".encode('ascii'))
+        s = clientsocket
+
+    while True:
+        Thread(target = listen(s,2048)).start()
+        Thread(target = send(s)).start()
+
